ab77b0419a502b4ef2144c852cc69cab77a6f991
toxufe <723887809@qq.com>
2017-02-09 23:40:23 +0800

    add

--- build/js/index.js
+++ build/js/index.js
@@ -22,3 +22,11 @@ angular.module('app').config(['$stateProvider','$urlRouterProvider',function($st
 angular.module('app').controller('mainCtrl',['$scope',function($scope){
 
 }]);
+'use strict';
+angular.module('app').directive('appHead',[function(){
+  return {
+    restrict:'A',   //利用属性调用指令
+    replace:true, //是否替换父级元素
+    templateUrl:'view/template/head.html'
+  };
+}]);
